fbca07a7c1f39fe5fcc965acf3e0d5f46f7f584f
Lisa Rastogi <lisarastogi@csu.fullerton.edu>
2017-05-04 17:16:53 -0700

    Added files, first commit

---
+++ Week15Homework.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+int SEARCH(int);
+
+int main() {
+
+int size = 5;
+arry[size] = {5,10, 15, 20, 25};
+
+search(5);
+
+
+
+    return 0;
+}
+
+int SEARCH(int m) {
+
+if(array[m] == target) {
+SEARCh(arr[], m+1, lo, hi, target)
+}
+
+if(arry[m] > target) {
+SEARCH(arr[]m m+1, lo, hi, target)
+}
+
+if(arry[m] < target)
+}
